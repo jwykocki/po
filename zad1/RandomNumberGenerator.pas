f8b46e2753397536910ffb8c4f406c4d0494f66d
Kuba <jakub.wykocki@wp.pl>
2025-03-28 23:35:09 +0100

    test and params in generator

--- zad1/RandomNumberGenerator.pas
+++ zad1/RandomNumberGenerator.pas
@@ -22,3 +22,46 @@ begin
     Write(arr[i], ' ');
   Writeln;
 end;
+
+procedure TestGenerateRandomNumbers(minVal, maxVal, count: Integer);
+var
+  arr: TIntArray;
+  i: Integer;
+  passed: Boolean;
+begin
+  GenerateRandomNumbers(arr, minVal, maxVal, count);
+  Writeln('Generated array:');
+  PrintArray(arr);
+
+  passed := True;
+
+  if (Length(arr) <> count) then
+  begin
+    passed := False;
+    Writeln('Generated array length not equal to count');
+  end;
+
+  for i := 0 to High(arr) do
+    if (arr[i] < minVal) or (arr[i] > maxVal) then
+    begin
+      passed := False;
+      Writeln('One of the array elements not in the range');
+      Break; 
+    end;
+
+  if passed then
+    Writeln('Test: PASSED')
+  else
+    Writeln('Test: FAILED');
+end;
+
+begin
+  Writeln('Running tests...');
+  TestGenerateRandomNumbers(1, 10, 20);
+  TestGenerateRandomNumbers(1, 100, 10);
+  TestGenerateRandomNumbers(1, 10, 0);
+  TestGenerateRandomNumbers(1, 10, 1);
+  TestGenerateRandomNumbers(1, 1, 100);
+  Writeln('Tests completed.');
+  Readln;
+end.
